b33cbf5f466c0dec99e5c8af3c049716f9647f7a
PUSH TO PROD <vaibhavkhartode001@gmail.com>
2026-09-21 19:07:16 +0530

    update: locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,11 +1,11 @@
 locals {
-  bucket_name_lower = lower(var.bucket_name)
-
   common_tags = {
-    Environment = var.environment
-    Project     = var.project
-    Owner       = var.owner
-    ManagedBy   = "terraform"
-    Name        = "Vaibhav-Khartode-AI-POC"
+    Environment  = var.environment
+    Project      = var.project
+    Owner        = var.owner
+    CostCenter   = var.cost_center
+    ManagedBy    = "terraform"
+    Terraform    = "true"
+    BusinessUnit = var.business_unit
   }
 }
