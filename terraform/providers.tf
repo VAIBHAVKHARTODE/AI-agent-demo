5f2b645b1f13f04deeaebf7112071dbbe42870b3
PUSH TO PROD <vaibhavkhartode001@gmail.com>
2026-09-21 17:14:36 +0530

    update: providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.3.0"
 
   required_providers {
     aws = {
@@ -8,7 +8,7 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.6"
+      version = "~> 3.5"
     }
   }
 }
