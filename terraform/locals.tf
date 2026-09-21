c384e0985e0d6a091997d036ce639992b061e8bb
PUSH TO PROD <vaibhavkhartode001@gmail.com>
2026-09-21 19:09:59 +0530

    Generate Infra: Create an S3 bucket with versioning, KMS encryption, and block all public access (#8)
    * generate: backend.tf
    
    * generate: data.tf
    
    * generate: locals.tf
    
    * generate: main.tf
    
    * generate: outputs.tf
    
    * generate: providers.tf
    
    * generate: variables.tf

---
+++ terraform/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  common_tags = {
+    Environment  = var.environment
+    Project      = var.project
+    Owner        = var.owner
+    CostCenter   = var.cost_center
+    ManagedBy    = "terraform"
+    Terraform    = "true"
+    BusinessUnit = var.business_unit
+  }
+}
