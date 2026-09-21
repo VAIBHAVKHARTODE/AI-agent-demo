083bb6f98275bc84f188d82491017e1d5c8dd651
PUSH TO PROD <vaibhavkhartode001@gmail.com>
2026-09-21 19:22:50 +0530

    Generate infra 1789998741 (#10)
    * generate: backend.tf
    
    * generate: data.tf
    
    * generate: locals.tf
    
    * generate: main.tf
    
    * generate: outputs.tf
    
    * generate: providers.tf
    
    * generate: variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Base name for the S3 bucket (a random suffix will be appended for global uniqueness)"
+  type        = string
+  default     = "vaibhav-khartode-ai-poc"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+  default     = "vaibhav-khartode-ai-poc"
+}
+
+variable "owner" {
+  description = "Owner of the resources"
+  type        = string
+  default     = "platform-team"
+}
+
+variable "kms_deletion_window_in_days" {
+  description = "Number of days before KMS key deletion"
+  type        = number
+  default     = 30
+}
+
+variable "enable_kms_key_rotation" {
+  description = "Whether to enable automatic KMS key rotation"
+  type        = bool
+  default     = true
+}
