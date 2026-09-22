c2d1639a264319dacf6b7eceefe017e7fff9d279
PUSH TO PROD <vaibhavkhartode001@gmail.com>
2026-09-22 13:12:53 +0530

    Generate Infra: Create an S3 bucket with versioning, KMS encryption, and block all public access (#15)
    * generate: backend.tf
    
    * generate: data.tf
    
    * generate: main.tf
    
    * generate: outputs.tf
    
    * generate: providers.tf
    
    * generate: variables.tf
    
    * update: backend.tf
    
    * update: data.tf
    
    * update: locals.tf
    
    * update: main.tf
    
    * update: outputs.tf
    
    * update: providers.tf
    
    * update: variables.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,3 +1,5 @@
+# Reference the existing KMS key alias instead of creating a new one,
+# since the alias already exists in the account.
 data "aws_kms_alias" "existing" {
   name = var.kms_alias_name
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "Name of the created S3 bucket"
+output "bucket_id" {
+  description = "The name of the S3 bucket"
   value       = aws_s3_bucket.this.id
 }
 
 output "bucket_arn" {
-  description = "ARN of the created S3 bucket"
+  description = "The ARN of the S3 bucket"
   value       = aws_s3_bucket.this.arn
 }
 
 output "kms_key_arn" {
-  description = "ARN of the existing KMS key used for bucket encryption"
+  description = "The ARN of the existing KMS key used for bucket encryption"
   value       = data.aws_kms_alias.existing.target_key_arn
 }
