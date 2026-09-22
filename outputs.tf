cc4b28bde81d3ed0797a691cd491b4198c6f309b
PUSH TO PROD <vaibhavkhartode001@gmail.com>
2026-09-22 14:27:40 +0530

    generate: outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,11 +9,6 @@ output "bucket_arn" {
 }
 
 output "kms_key_arn" {
-  description = "The ARN of the KMS key used for bucket encryption"
-  value       = aws_kms_key.s3_key.arn
-}
-
-output "kms_key_alias" {
-  description = "The alias of the KMS key used for bucket encryption"
-  value       = aws_kms_alias.s3_key_alias.name
+  description = "The ARN of the existing KMS key used for bucket encryption"
+  value       = data.aws_kms_alias.existing.target_key_arn
 }
