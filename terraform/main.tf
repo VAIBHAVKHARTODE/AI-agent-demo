5e22edc5ca6f380b2abce1f46858b0496dc0c8a5
PUSH TO PROD <vaibhavkhartode001@gmail.com>
2026-09-22 14:01:39 +0530

    generate: main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,23 @@
+resource "aws_kms_key" "s3_key" {
+  description             = "KMS key for encrypting ${var.bucket_name} S3 bucket"
+  deletion_window_in_days = var.kms_deletion_window_in_days
+  enable_key_rotation     = true
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.bucket_name}-kms-key"
+    }
+  )
+}
+
+resource "aws_kms_alias" "s3_key_alias" {
+  name          = "alias/${var.bucket_name}-key"
+  target_key_id = aws_kms_key.s3_key.key_id
+}
+
 resource "aws_s3_bucket" "this" {
-  bucket        = var.bucket_name
-  force_destroy = false
+  bucket = var.bucket_name
 
   tags = merge(
     local.common_tags,
@@ -24,7 +41,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
-      kms_master_key_id = data.aws_kms_alias.existing.target_key_arn
+      kms_master_key_id = aws_kms_key.s3_key.arn
     }
     bucket_key_enabled = true
   }
