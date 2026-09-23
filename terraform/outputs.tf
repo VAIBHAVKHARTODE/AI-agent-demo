be03b30618ce65d36522fb7c0a9a9acf9ababc37
PUSH TO PROD <vaibhavkhartode001@gmail.com>
2026-09-23 12:12:07 +0530

    generate: outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "bucket_id" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.this.id
+}
+
+output "bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.this.arn
+}
+
+output "kms_key_id" {
+  description = "The ID of the KMS key used for bucket encryption"
+  value       = aws_kms_key.s3.key_id
+}
+
+output "kms_key_arn" {
+  description = "The ARN of the KMS key used for bucket encryption"
+  value       = aws_kms_key.s3.arn
+}
+
+output "kms_alias_name" {
+  description = "The alias of the KMS key used for bucket encryption"
+  value       = aws_kms_alias.s3.name
+}
