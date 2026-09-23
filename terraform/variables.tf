245704ab11a312cef005b4a0dbd48f9db7cca008
PUSH TO PROD <vaibhavkhartode001@gmail.com>
2026-09-23 12:12:11 +0530

    generate: variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,59 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project" {
+  description = "Project name used for naming and tagging"
+  type        = string
+  default     = "schoolspider"
+}
+
+variable "environment" {
+  description = "Environment name (dev, test, qa, uat, stage, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "owner" {
+  description = "Owner tag value"
+  type        = string
+  default     = "platform-team"
+}
+
+variable "cost_center" {
+  description = "CostCenter tag value"
+  type        = string
+  default     = "unassigned"
+}
+
+variable "business_unit" {
+  description = "BusinessUnit tag value"
+  type        = string
+  default     = "unassigned"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "schoolspider-dev-app-data"
+}
+
+variable "kms_deletion_window_in_days" {
+  description = "Waiting period before KMS key deletion"
+  type        = number
+  default     = 30
+}
+
+variable "enable_kms_key_rotation" {
+  description = "Enable automatic KMS key rotation"
+  type        = bool
+  default     = true
+}
+
+variable "force_destroy" {
+  description = "Allow bucket to be destroyed even if it contains objects"
+  type        = bool
+  default     = false
+}
