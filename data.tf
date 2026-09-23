ae1d81e7ca11c332d1b0d2ad63a87d6d898b0e8b
PUSH TO PROD <vaibhavkhartode001@gmail.com>
2026-09-23 14:47:00 +0530

    update: data.tf

--- data.tf
+++ data.tf
@@ -13,13 +13,18 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
+data "aws_vpc" "selected" {
+  id      = var.vpc_id != "" ? var.vpc_id : null
+  default = var.vpc_id == "" ? true : null
 }
 
-data "aws_subnets" "default" {
+data "aws_subnets" "selected" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+    values = [data.aws_vpc.selected.id]
   }
+}
+variable "vpc_id" {
+  type    = string
+  default = "example"
 }
